8cad9e9f3a464f70f2edcf70ed72e8d0c74299b5
PrafulGupta <guptapraful130@gmail.com>
2025-05-04 03:24:38 +0530

    Initial commit after clearing previous Git setup

---
+++ frontend/src/constants.js
@@ -0,0 +1,308 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+import flaskLogo from "./assets/tech_logo/flaskLogo.svg";
+import rasaLogo from "./assets/tech_logo/rasaLogo.png";
+import langchainLogo from "./assets/tech_logo/langchainLogo.png";
+import dockerLogo from "./assets/tech_logo/dockerLogo.png";
+import jestLogo from "./assets/tech_logo/jestLogo.png";
+import mochaLogo from "./assets/tech_logo/mochaLogo.png";
+import chaiLogo from "./assets/tech_logo/chaiLogo.jpeg";
+import redisLogo from "./assets/tech_logo/redisLogo.png";
+import GFGLogo from "./assets/tech_logo/GFGLogo.png";
+import LeetCodeLogo from "./assets/tech_logo/LeetCodeLogo.png";
+import ApexSF from "./assets/tech_logo/ApexSF.png";
+import SFLogo from "./assets/tech_logo/SFLogo.jpeg";
+
+// Experience Section Logo's
+import webverseLogo from "./assets/company_logo/webverse_logo.png";
+import searchunifylogo from "./assets/company_logo/searchunify_logo.jpeg";
+import agcLogo from "./assets/company_logo/agc_logo.png";
+import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+
+// Education Section Logo's
+import glaLogo from "./assets/education_logo/gla_logo.png";
+import bsaLogo from "./assets/education_logo/bsa_logo.png";
+import vpsLogo from "./assets/education_logo/vps_logo.png";
+import coerLogo from "./assets/education_logo/COERLogo.jpeg";
+import SchoolLogo from "./assets/education_logo/SchoolLogo.jpeg";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import csprepLogo from "./assets/work_logo/cs_prep.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import removebgLogo from "./assets/work_logo/remove_bg.png";
+
+export const SkillsInfo = [
+  {
+    title: "Languages & Scripting",
+    skills: [
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      // { name: "Java", logo: javaLogo },
+      { name: "Python", logo: pythonLogo },
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "Salesforce APEX", logo: ApexSF },
+    ],
+  },
+  {
+    title: "Problem Solving",
+    skills: [
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "LeetCode", logo: LeetCodeLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "GeeksForGeeks", logo: GFGLogo },
+    ],
+  },
+  {
+    title: "Frameworks & Libraries",
+    skills: [
+      { name: "React JS", logo: reactjsLogo },
+      // { name: "Redux", logo: reduxLogo },
+      // { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "Flask", logo: flaskLogo },
+      { name: "LangChain", logo: langchainLogo },
+      { name: "Rasa", logo: rasaLogo },
+      { name: "Salesforce LWC", logo: SFLogo },
+    ],
+  },
+  {
+    title: "Tools & Databases:",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "MongoDB Compass", logo: mcLogo },
+      { name: "Docker", logo: dockerLogo },
+      { name: "Jest", logo: jestLogo },
+      { name: "Chai", logo: chaiLogo },
+      { name: "Mocha", logo: mochaLogo },
+      { name: "Figma", logo: figmaLogo },
+      // { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      // { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      // { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+      { name: "Redis", logo: redisLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: searchunifylogo,
+    role: "Software Engineer",
+    company: "SearchUnify(Grazitti Interactive)",
+    date: "September 2024 - Present",
+    desc: "Worked on scalable and high-performance product development for the Knowbler platform, implementing features for integration with platforms like ServiceNow and Salesforce. Utilized technologies like Node.js, Express, React, and PostgreSQL to build efficient, responsive solutions. Focused on maintaining production-level quality, deploying features using Docker, and ensuring reliability through automated testing with Jest, Mocha, and Chai.",
+    skills: [
+      "Node JS",
+      "Express JS",
+      "Salesforce LWC",
+      "Salesforce AURA",
+      "React JS",
+      "SCSS",
+      "PostgreSQL",
+      "MongoDB",
+      "Docker",
+      "Jest",
+      "Mocha",
+      "Chai",
+    ],
+  },
+  {
+    id: 1,
+    img: searchunifylogo,
+    role: "Software Engineer Intern",
+    company: "SearchUnify(Grazitti Interactive)",
+    date: "March 2024 - August 2024",
+    desc: "Contributed to the development and optimization of backend systems for the SUVA chatbot. Applied technologies like Python, Flask, Rasa, and Redis to implement scalable solutions. Focused on improving production workflows, utilizing Docker for containerization, and optimizing data management with MongoDB and PostgreSQL. Ensured feature stability and performance in live environments by automating deployment pipelines and reducing redundant tasks of training and publishing.",
+    skills: [
+      "Python",
+      "Flask",
+      "Rasa",
+      "Redis",
+      "PostgreSQL",
+      "MongoDB",
+      "Langchain",
+      "Docker",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: coerLogo,
+    school: "College of Engineering Roorkee",
+    date: "September 2020 - June 2024",
+    grade: "80.83%",
+    desc: "I completed my Bachelor's degree in Information Technology from the prestigious College of Engineering Roorkee. During my academic tenure, I excelled in various subjects such as Data Structures, Algorithms, Web Development, and Object-Oriented Programming. I actively participated in college-level hackathons, securing 2nd place in the 36-hr Smart India Hackathon and 48-hr College Organized Hackathon and 3rd place in the college-level DSA contest. As the Placement Coordinator, I led multiple placement drives, successfully managing students and facilitating the recruitment process. My time at CoER has honed my technical and leadership skills, making me well-prepared for the professional world.",
+    degree: "Bachelor of Technology - B.Tech (Information Technology)",
+  },
+  {
+    id: 1,
+    img: SchoolLogo,
+    school: "Guru Nanak Modern School",
+    date: "April 2018 - March 2019",
+    grade: "88.83%",
+    desc: "I completed my Class 12 education at Guru Nanak Modern School, Kanpur, under the CBSE board, specializing in Physics, Chemistry, Mathematics (PCM). My academic excellence was complemented by a leadership role as the Deputy Head Boy, where I demonstrated responsibility and effective communication skills. I also excelled in extracurricular activities, a team player in the school’s volleyball team and securing runner-up in an inter-school chess competition.",
+    degree: "CBSE (XII) - PCM",
+  },
+  {
+    id: 2,
+    img: SchoolLogo,
+    school: "Guru Nanak Modern School",
+    date: "April 2016 - March 2017",
+    grade: "96%",
+    desc: "I completed my Class 10 education at Guru Nanak Modern School with a score of 96%, excelling in all subjects. My time in school saw me participating in various competitions, including debates and sports. I was actively involved in organizing events and was recognized for my leadership and teamwork. These experiences laid the groundwork for my future endeavors in both academics and extracurricular activities.",
+    degree: "CBSE(X) - All Subjects",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "GitHub Profile Detective",
+    description:
+      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    image: githubdetLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github:
+      "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "CS Prep",
+    description:
+      "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+    image: csprepLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    github: "https://github.com/codingmastr/CSPrep",
+    webapp: "https://csprep.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Movie Recommendation App",
+    description:
+      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    image: movierecLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/codingmastr/Movie-Recommendation-App",
+    webapp: "https://movie-recommendation-app-jet.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Email Validator NPM Package",
+    description:
+      "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+    image: npmLogo,
+    tags: ["React JS", "Node.js", "NPM", "Validation"],
+    github: "https://github.com/codingmastr/cmtk-email-validator",
+    webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+  },
+  {
+    id: 4,
+    title: "Task Reminder Chrome Extension Tool",
+    description:
+      "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    image: taskremLogo,
+    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    github: "https://github.com/codingmastr/Task-Reminder-Tool",
+    webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+  },
+  {
+    id: 5,
+    title: "Webverse Digital",
+    description:
+      "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    image: webverLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    github: "https://github.com/codingmastr/Webverse-Digital",
+    webapp: "https://webversedigital.com/",
+  },
+  {
+    id: 6,
+    title: "Coding Master",
+    description:
+      "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    image: cmLogo,
+    tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    github: "https://codingmasterweb.in/",
+    webapp: "https://codingmasterweb.in/",
+  },
+  {
+    id: 7,
+    title: "Image Search App",
+    description:
+      "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    image: imagesearchLogo,
+    tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    github: "https://github.com/codingmastr/Image-Search-App",
+    webapp: "https://imagsearch.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Image Background Remover",
+    description:
+      "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    image: removebgLogo,
+    tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    github: "https://github.com/codingmastr/Image-Background-Remover",
+    webapp: "https://removeyourbg.netlify.app/",
+  },
+];
